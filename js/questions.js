c83902b5db84b8f59152f41ec826213fab3dab4e
Rifqi Pratama <rifqipratama9@gmail.com>
2022-02-15 23:15:03 +0700

    add soal page, materi page and change video

---
+++ js/questions.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Arti dari aksara sunda ini adalah ᮝᮤᮜᮥᮏᮨᮀ ᮞᮥᮙ᮪ᮕᮤᮀ?",
+    answer: "Wilujeung Sumping",
+    options: [
+      "Wilujeung Enjing",
+      "Wilujeung Sumping",
+      "Wilujeung Tepang",
+      "Wilujeung Siang",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Arti dari aksara sunda ini  ᮙᮔᮨᮂ?",
+    answer: "Maneh",
+    options: ["Mamam", "Mamah", "Maneh", "Manah"],
+  },
+  {
+    numb: 3,
+    question: "Aksara sunda “Bandung” ?",
+    answer: "ᮘᮔ᮪ᮓᮥᮀ",
+    options: ["ᮘᮔ᮪ᮓᮥᮀ", "ᮘᮔ᮪ᮒᮨᮔ᮪", "ᮘᮔ᮪ᮏᮁ", "ᮘᮓᮥᮀ"],
+  },
+  {
+    numb: 4,
+    question: "Arti dari aksara sunda ini ᮏᮝ ᮘᮛᮒ᮪?",
+    answer: "Jawa Barat",
+    options: ["Jawa Timur", "Jakarta", "Jawa Barat", "Jawa Tengah"],
+  },
+  {
+    numb: 5,
+    question: "Aksara sunda “2022” ?",
+    answer: "|᮲᮰᮲᮲|",
+    options: ["|᮲᮰᮱᮲|", "|᮲᮰᮲᮳|", "|᮲᮰᮴᮲|", "|᮲᮰᮲᮲|"],
+  },
+  {
+    numb: 6,
+    question: "Aksara sunda “keur naon?” ?",
+    answer: "ᮊᮩᮁ ᮔᮇᮔ᮪ ?",
+    options: ["ᮊᮩᮁ ᮙᮔᮨᮂ ?", "ᮊᮩᮁ ᮃᮔ᮪ᮏᮩᮔ᮪ ?", "ᮊᮩᮁ ᮍᮧᮕᮤ ?", "ᮊᮩᮁ ᮔᮇᮔ᮪ ?"],
+  },
+  {
+    numb: 7,
+    question: "Aksara sunda |᮲᮴᮶᮷᮶| ",
+    answer: "24676",
+    options: ["27676", "24676", "26476", "24766"],
+  },
+  {
+    numb: 8,
+    question: "Aksara sunda “Urang Sumedang Asli” ?",
+    answer: "ᮅᮛᮀ ᮞᮥᮙᮨᮓᮀ ᮃᮞᮣᮤ",
+    options: [
+      "ᮅᮛᮀ ᮞᮥᮙᮨᮓᮀ ᮃᮞᮣᮤ",
+      "ᮅᮛᮀ ᮞᮥᮘᮀ ᮃᮞᮣᮤ",
+      "ᮅᮛᮀ ᮞᮨᮙᮛᮀ ᮃᮞᮣᮤ",
+      "ᮅᮛᮀ ᮞᮥᮓᮤᮙᮙ᮪ᮕᮤᮁ ᮃᮞᮣᮤ",
+    ],
+  },
+  {
+    numb: 9,
+    question: "ᮎᮤᮊᮛᮎᮊ᮪ ᮔᮤᮀᮌᮀ ᮘᮒᮥ ᮜᮅᮔ᮪-ᮜᮅᮔ᮪ ᮏᮓᮤ ?",
+    answer: "ᮜᮨᮌᮧᮊ᮪",
+    options: ["ᮎᮤᮎᮤᮀ", "ᮜᮨᮊᮣᮧᮊ᮪", "ᮜᮨᮘᮧᮊ᮪", "ᮜᮨᮌᮧᮊ᮪"],
+  },
+  {
+    numb: 10,
+    question: "ᮄᮘᮥ ᮊᮧᮒ ᮄᮔ᮪ᮓᮧᮔᮨᮞᮤᮃ ?",
+    answer: "ᮏᮊᮁᮒ",
+    options: ["ᮏᮧᮌ᮪ᮏᮊᮁᮒ", "ᮏᮙ᮪ᮘᮤ", "ᮏᮊᮁᮒ", "ᮘᮔ᮪ᮓᮥᮀ"],
+  },
+];
--- js/script.js
+++ js/script.js
@@ -5,3 +5,49 @@ const navbarMenu = document.querySelector(".navbar-menu");
 mobileMenu.addEventListener("click", function () {
   navbarMenu.classList.toggle("show");
 });
+
+
+//example
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("demo");
+  const captionText = document.getElementById("caption");
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " active";
+  // captionText.innerHTML = dots[slideIndex-1].alt;
+}
