0b6305d1897a15bf12a98cc6d143653acec9ee24
Rifqi Pratama <rifqipratama9@gmail.com>
2022-02-15 23:29:44 +0700

    redirect exit page soal to homepage

--- js/sc.js
+++ js/sc.js
@@ -17,7 +17,8 @@ start_btn.onclick = () => {
 
 // if exitQuiz button clicked
 exit_btn.onclick = () => {
-  info_box.classList.remove("activeInfo"); //hide info box
+  // info_box.classList.remove("activeInfo"); //hide info box
+  window.location.href = "/"; // return to homepage
 };
 
 // if continueQuiz button clicked
@@ -62,7 +63,8 @@ restart_quiz.onclick = () => {
 
 // if quitQuiz button clicked
 quit_quiz.onclick = () => {
-  window.location.reload(); //reload the current window
+  // window.location.reload(); //reload the current window
+  window.location.href = "/"; // return to homepage
 };
 
 const next_btn = document.querySelector("footer .next_btn");
